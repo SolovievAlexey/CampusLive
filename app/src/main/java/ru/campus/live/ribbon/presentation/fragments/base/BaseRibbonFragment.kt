9048d7703bf36c2a5902b736b58f107d480e2212
alex <soloviev_alexey@bk.ru>
2022-04-19 21:09:41 +0300

    Переименовал класс

--- app/src/main/java/ru/campus/live/ribbon/presentation/fragments/base/BaseRibbonFragment.kt
+++
@@ -1,9 +0,0 @@
-package ru.campus.live.ribbon.presentation.fragments.base
-
-import ru.campus.live.core.presentation.ui.BaseFragment
-import ru.campus.live.databinding.FragmentFeedBinding
-
-class BaseRibbonFragment: BaseFragment<FragmentFeedBinding>() {
-
-    override fun getViewBinding() = FragmentFeedBinding.inflate(layoutInflater)
-}
