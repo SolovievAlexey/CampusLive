a7623fefbb6cf55004af546a64eb4c0d68e71bbb
alex <soloviev_alexey@bk.ru>
2022-04-23 22:47:31 +0300

    Реализовал кеширование и подгрузку кеша

--- app/src/main/java/ru/campus/live/ribbon/presentation/viewmodel/RibbonViewModel.kt
+++ app/src/main/java/ru/campus/live/ribbon/presentation/viewmodel/RibbonViewModel.kt
@@ -1,6 +1,5 @@
 package ru.campus.live.ribbon.presentation.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -18,11 +17,10 @@ import javax.inject.Inject
 
 class RibbonViewModel @Inject constructor(
     private val dispatchers: IDispatchers,
-    private val interactor: RibbonInteractor,
+    private val interactor: RibbonInteractor
 ) : ViewModel() {
 
     private var isLazyDownloadFeed = false
-    private var isDataCash = false
 
     private val listLiveData = MutableLiveData<ArrayList<RibbonModel>>()
     val list: LiveData<ArrayList<RibbonModel>>
@@ -36,17 +34,12 @@ class RibbonViewModel @Inject constructor(
     val complaintEvent: LiveData<RibbonModel>
         get() = complaintLiveData
 
-    init {
-        getCash()
-    }
+    init { getCash() }
 
     private fun getCash() {
         viewModelScope.launch(dispatchers.io) {
             val result = interactor.getCash()
-            if(result.size > 1) isDataCash = true
-            withContext(dispatchers.main) {
-                listLiveData.value = result
-            }
+            withContext(dispatchers.main) { listLiveData.value = result }
             get(refresh = true)
         }
     }
@@ -56,16 +49,12 @@ class RibbonViewModel @Inject constructor(
         viewModelScope.launch(dispatchers.io) {
             val model = ArrayList<RibbonModel>()
             listLiveData.value?.let { model.addAll(it) }
-
             val offset = interactor.getOffset(refresh, model)
             val result = interactor.get(offset = offset, model = model)
-
             val error = interactor.isErrorView(model = result)
             if(offset == 0 && !error) interactor.postCash(result)
             isLazyDownloadFeed = interactor.lazyDownloadFeed(result)
-
             val response = interactor.map(result)
-
             withContext(dispatchers.main) {
                 listLiveData.value = response
             }
