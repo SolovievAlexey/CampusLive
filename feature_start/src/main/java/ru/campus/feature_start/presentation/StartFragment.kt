55c9ac03b4f14c172e569f96afd3e71bbe6c68fd
alex <soloviev_alexey@bk.ru>
2022-05-12 23:26:26 +0300

    create StartBindModule

--- feature_start/src/main/java/ru/campus/feature_start/presentation/StartFragment.kt
+++ feature_start/src/main/java/ru/campus/feature_start/presentation/StartFragment.kt
@@ -1,34 +1,25 @@
 package ru.campus.feature_start.presentation
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.viewModels
 import ru.campus.core.di.AppDepsProvider
 import ru.campus.core.presentation.BaseFragment
 import ru.campus.feature_start.databinding.FragmentStartBinding
+import ru.campus.feature_start.di.DaggerStartComponent
 import ru.campus.feature_start.di.StartComponent
 
 
 class StartFragment : BaseFragment<FragmentStartBinding>() {
 
-    /*
     private val component: StartComponent by lazy {
         DaggerStartComponent.builder()
             .deps(AppDepsProvider.deps)
             .build()
     }
 
-
-
     private val viewModel by viewModels<StartViewModel> {
         component.viewModelsFactory()
     }
 
-     */
-
     override fun getViewBinding() = FragmentStartBinding.inflate(layoutInflater)
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
 
 }
