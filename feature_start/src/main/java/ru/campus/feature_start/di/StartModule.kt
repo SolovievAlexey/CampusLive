26458f574e93ef381ea557ea6f418befd7d5642a
alex <soloviev_alexey@bk.ru>
2022-05-12 23:22:17 +0300

    create StartBindModule

--- feature_start/src/main/java/ru/campus/feature_start/di/StartModule.kt
+++ feature_start/src/main/java/ru/campus/feature_start/di/StartModule.kt
@@ -1,20 +1,10 @@
 package ru.campus.feature_start.di
 
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
-import ru.campus.core.data.BaseResourceManager
 import ru.campus.core.data.BaseUserDataStore
-import ru.campus.core.data.ResourceManager
-import ru.campus.core.data.UserDataStore
 import ru.campus.feature_start.data.interfaces.APIService
-import ru.campus.feature_start.data.repository.BaseStartRepository
-import ru.campus.feature_start.data.repository.BaseUserRepository
-import ru.campus.feature_start.data.repository.StartRepository
-import ru.campus.feature_start.data.repository.UserRepository
-import ru.campus.feature_start.domain.BaseStartInteractor
-import ru.campus.feature_start.domain.StartInteractor
 
 /**
  * @author Soloviev Alexey
@@ -37,23 +27,4 @@ class StartModule {
 
 }
 
-@Module
-interface StartBindModule {
-
-    @Binds
-    fun bindResourceManager(resourceManager: BaseResourceManager): ResourceManager
-
-    @Binds
-    fun bindStartRepository(startRepository: BaseStartRepository): StartRepository
-
-    @Binds
-    fun bindStartInteractor(startInteractor: BaseStartInteractor): StartInteractor
-
-    @Binds
-    fun bindUserRepository(baseUserRepository: BaseUserRepository): UserRepository
-
-    @Binds
-    fun bindUserDataStore(baseUserDataStore: BaseUserDataStore): UserDataStore
-
-}
 
