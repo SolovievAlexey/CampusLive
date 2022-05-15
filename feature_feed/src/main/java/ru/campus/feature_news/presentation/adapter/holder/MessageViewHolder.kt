880c1cbcfa16260819b541a1b265cc0d6ae69455
alex <soloviev_alexey@bk.ru>
2022-05-15 19:12:27 +0300

    item message

---
+++ feature_feed/src/main/java/ru/campus/feature_news/presentation/adapter/holder/MessageViewHolder.kt
@@ -0,0 +1,19 @@
+package ru.campus.feature_news.presentation.adapter.holder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import ru.campus.feature_news.data.FeedModel
+
+/**
+ * @author Soloviev Alexey
+ * @contacts soloviev@internet.ru
+ * @date 15.05.2022 19:01
+ */
+
+class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(model: FeedModel) {
+
+    }
+
+}
