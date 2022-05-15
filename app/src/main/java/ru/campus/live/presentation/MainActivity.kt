5f5459596af5a5cd1c1a2c8942521bc1d6686e13
alex <soloviev_alexey@bk.ru>
2022-05-15 16:59:07 +0300

    bottomView navigation impl

--- app/src/main/java/ru/campus/live/presentation/MainActivity.kt
+++ app/src/main/java/ru/campus/live/presentation/MainActivity.kt
@@ -3,10 +3,14 @@ package ru.campus.live.presentation
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import ru.campus.core.data.UserDataStore
 import ru.campus.core.di.AppDepsProvider
 import ru.campus.live.R
+import ru.campus.live.databinding.ActivityMainBinding
 import ru.campus.live.di.DaggerMainComponent
 import ru.campus.live.di.MainComponent
 import javax.inject.Inject
@@ -21,10 +25,13 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var userDataSource: UserDataStore
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         component.inject(this)
         if (userDataSource.token().isEmpty() || userDataSource.location() == 0) {
             val intent = Intent(this, LoginActivity::class.java)
@@ -34,6 +41,7 @@ class MainActivity : AppCompatActivity() {
             val navHostFragment =
                 supportFragmentManager.findFragmentById(R.id.mainNavigationHost) as NavHostFragment
             val navController = navHostFragment.navController
+            binding.bottomNavigationView.setupWithNavController(navController)
         }
     }
 
