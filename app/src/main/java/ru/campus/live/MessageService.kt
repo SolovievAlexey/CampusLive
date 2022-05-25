13da02746d54d3b0cc6cc5503cb7383d222e2243
alex <soloviev_alexey@bk.ru>
2022-05-25 23:08:10 +0300

    implementation MessageService

--- app/src/main/java/ru/campus/live/MessageService.kt
+++ app/src/main/java/ru/campus/live/MessageService.kt
@@ -1,7 +1,18 @@
 package ru.campus.live
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import ru.campus.live.presentation.MainActivity
 
 /**
  * @author Soloviev Alexey
@@ -9,14 +20,57 @@ import com.google.firebase.messaging.RemoteMessage
  * @date 25.05.2022 22:48
  */
 
-class MessageService: FirebaseMessagingService() {
+private const val CHANNEL_ID = 8473943.toString()
 
+class MessageService : FirebaseMessagingService() {
+
+    private val sPref: SharedPreferences =
+        applicationContext.getSharedPreferences("AppDB", Context.MODE_PRIVATE)
+
+    @SuppressLint("UnspecifiedImmutableFlag")
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        createNotificationChannel()
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(message.data["title"])
+            .setContentText(message.data["body"])
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(System.currentTimeMillis().toInt(), builder.build())
+        }
     }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+        sPref.edit().putString("firebase_token", token).apply()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val createNotificationChannel = sPref.getBoolean("createNotificationChannel", false)
+            if (createNotificationChannel) return
+            sPref.edit().putBoolean("createNotificationChannel", true).apply()
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
 }
