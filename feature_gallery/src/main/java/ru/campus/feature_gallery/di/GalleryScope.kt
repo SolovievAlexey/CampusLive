47272449c67acb63bcd2c184f98de2bac19d307d
alex <soloviev_alexey@bk.ru>
2022-05-26 21:45:51 +0300

    GalleyScope

---
+++ feature_gallery/src/main/java/ru/campus/feature_gallery/di/GalleryScope.kt
@@ -0,0 +1,12 @@
+package ru.campus.feature_gallery.di
+
+import javax.inject.Scope
+
+/**
+ * @author Soloviev Alexey
+ * @contacts soloviev@internet.ru
+ * @date 26.05.2022 21:44
+ */
+
+@Scope
+annotation class GalleryScope
