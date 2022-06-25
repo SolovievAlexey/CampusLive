7de19794c4e34071400731780e3f51bf0d927fee
alex <soloviev_alexey@bk.ru>
2022-06-25 22:53:38 +0300

    Icon appearance animation

--- feature_start/src/main/java/ru/campus/feature_start/presentation/adapter/holder/StartViewHolder.kt
+++ feature_start/src/main/java/ru/campus/feature_start/presentation/adapter/holder/StartViewHolder.kt
@@ -1,7 +1,9 @@
 package ru.campus.feature_start.presentation.adapter.holder
 
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import ru.campus.feature_start.R
 import ru.campus.feature_start.data.model.StartModel
 import ru.campus.feature_start.databinding.ItemPresentationBinding
 
@@ -11,14 +13,22 @@ import ru.campus.feature_start.databinding.ItemPresentationBinding
  * @date 13.05.2022 20:01
  */
 
-class StartViewHolder(private val itemBinding: ItemPresentationBinding) :
-    RecyclerView.ViewHolder(itemBinding.root) {
+class StartViewHolder(
+    private val itemBinding: ItemPresentationBinding
+) : RecyclerView.ViewHolder(itemBinding.root) {
+
+    private val context = itemBinding.root.context
 
     fun bind(item: StartModel) {
         itemBinding.title.text = item.title
         itemBinding.message.text = item.message
-        val context = itemBinding.root.context
         Glide.with(context).load(item.icon).into(itemBinding.icon)
+
+        if (layoutPosition == 0 && !item.animation) {
+            item.animation = true
+            val animation = AnimationUtils.loadAnimation(context, R.anim.appearance)
+            itemBinding.icon.startAnimation(animation)
+        }
     }
 
 }
